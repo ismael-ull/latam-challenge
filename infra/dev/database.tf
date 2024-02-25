1a84bf6e18b4634787bfd2ad3f235252bb934168
Ismael Ull <ismael.ull@bluon.cloud>
2024-02-25 10:47:22 -0300

    Changes in Secrets and DB

--- infra/dev/database.tf
+++ infra/dev/database.tf
@@ -1,16 +1,16 @@
 module "lc-db" {
   source     = "../cff-modules/cloudsql-instance"
   project_id = var.project
-  name             = "lc-schema"
+  name             = "lc-db-01"
   region           = var.region
   database_version = "MYSQL_8_0"
   tier             = "db-g1-small"
+  deletion_protection = false
   network_config = {
     connectivity = {
       public_ipv4 = true      
     }
-   users = {
-    # generatea password for user1
+   users = {    
     lcadmin = {
       password = module.secret-manager-backend.ids["CLOUSQL_PASSWD"]
     }
--- infra/dev/serverless.tf
+++ infra/dev/serverless.tf
@@ -2,11 +2,45 @@ module "secret-manager-backend" {
   source     = "../cff-modules/secret-manager"
   project_id = var.project
   secrets = {
-    CLOUSQL_PASSWD = { data = "Passw0rd"}
+    CLOUSQL_PASSWD = {}
+  }
+  versions = {
+    CLOUSQL_PASSWD = {
+      v1 = {enabled = true, data = "Passw0rd"}
+    }
   }
   iam = {
     CLOUSQL_PASSWD = {
       "roles/secretmanager.secretAccessor" = ["serviceAccount:${module.sa-backend.email}"]
     }
   }
+}
+
+module "cloud_run" {
+  source     = "../cff-modules/cloud-run"
+  project_id = var.project
+  region     = var.region
+  name       = "lc-backend"
+  containers = {
+    hello = {
+      image = "us-docker.pkg.dev/cloudrun/container/hello"
+      env = {
+        PROJECT_ID = var.project,
+        CLOUDSQL_INSTANCE_NAME = "VALUE2",
+        DATABASE_NAME = "VALUE3",
+        DATABASE_SCHEMA = "VALUE4",
+        DATABASE_USERNAME = "VALUE5"
+      }
+      env_from = {
+        SECRET1 = {
+          name = module.secret-manager-backend.ids["CLOUSQL_PASSWD"]
+          key  = "latest"
+        }
+      }
+    }
+  }
+  iam = {
+    "roles/run.invoker" = ["allUsers"]
+  }
+  service_account_create = true
 }
