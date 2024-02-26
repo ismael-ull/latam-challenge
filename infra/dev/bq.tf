49b553516023116254df9008ef4ed069b043238f
Ismael Ull <ismael.ull@bluon.cloud>
2024-02-26 09:15:48 -0300

    PubSub Subscription

--- infra/dev/bq.tf
+++ infra/dev/bq.tf
@@ -1,9 +1,27 @@
 
+locals {
+  lc-db-schema = jsonencode([
+    { name = "ID", type = "INT" },
+    { name = "Name", type = "STRING" },
+    { name = "LastName", type = "STRING" },
+    { name = "FieldA", type = "STRING" },
+    { name = "FieldB", type = "STRING" },
+    { name = "FieldC", type = "STRING" }
+  ])
+}
+
 module "bigquery-lc" {
   source     = "../cff-modules/bigquery-dataset"
   project_id = var.project
   id         = "lc_bq_dataset"
-  iam = {
+  /**iam = {
     "roles/bigquery.dataOwner" = ["serviceAccount:${module.sa-pubsub.email}"]
   }
+  tables = {
+    lc-bq-table = {
+      friendly_name       = "lc-bq-table"
+      schema              = local.lc-db-schema
+      deletion_protection = true
+    }
+  }**/
 }
--- infra/dev/pubsub.tf
+++ infra/dev/pubsub.tf
@@ -1,4 +1,4 @@
-module "pubsub" {
+/**module "pubsub" {
   source     = "../cff-modules/pubsub"
   project_id = var.project
   name       = "lc-bq-update"
@@ -6,4 +6,14 @@ module "pubsub" {
   //  "roles/pubsub.viewer"     = ["group:foo@example.com"]
   //  "roles/pubsub.subscriber" = ["user:user1@example.com"]
   //}
-}
+  subscriptions = {
+    lc-bq-update-subs = {
+      bigquery = {
+        table               = "${var.project}:${module.bigquery-lc.dataset_id}.lc-bq-table"
+        use_topic_schema    = true
+        write_metadata      = false
+        drop_unknown_fields = true
+      }
+    }
+  }
+}**/
